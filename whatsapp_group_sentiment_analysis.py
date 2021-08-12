4666cf983f224f98352f8b2c89b5504ecbe9135e
yaniv92648 <yaniv92648@gmail.com>
2021-08-12 15:20:37 +0300

    Update whatsapp_group_sentiment_analysis.py

--- whatsapp_group_sentiment_analysis.py
+++ whatsapp_group_sentiment_analysis.py
@@ -5,15 +5,16 @@ from transformers import AutoTokenizer, AutoModel, pipeline
 from collections import defaultdict
 
 
-# Load the file to total_chat
-with open('FILE_NAME.txt', encoding="utf-8") as file:
-  total_chat = file.readlines()
+def get_total_chat(filename):
+  with open(filename, encoding="utf-8") as file:
+    return file.readlines()
   
   
-# Create a people dictionary of lists where every person's name is the key 
-# and the list of his msgs is the value
-people = defaultdict(list)
-for l in total_chat:
+def get_people(total_chat):
+  # Create a people dictionary of lists where every person's name is the key 
+  # and the list of his msgs is the value
+  people = defaultdict(list)
+  for l in total_chat:
   sen = l.rstrip("\n")
   if ']' in sen:
     name_and_msg = sen.split(']')[1]
@@ -23,20 +24,20 @@ for l in total_chat:
       msg = name_and_msg.split(':')[1]
       people[name].append(msg)
 
-# Clean noise made by the subject of the group
-for person, msgs in people.copy().items():
+  # Clean noise made by the subject of the group
+  for person, msgs in people.copy().items():
   if len(msgs) < 5:
     people.pop(person)
 
-# Get the no. of msgs of the person with the fewest no. of msgs
-min_msgs = np.min([len(msgs) for person, msgs in people.items()])
+  # Get the no. of msgs of the person with the fewest no. of msgs
+  min_msgs = np.min([len(msgs) for person, msgs in people.items()])
 
-# Align everyone's no. of msgs to his no. of msgs so we can put in a dataframe
-for person, msgs in people.items():
+  # Align everyone's no. of msgs to his no. of msgs so we can put in a dataframe
+  for person, msgs in people.items():
   while len(msgs) > min_msgs:
     msgs.pop()
 
-df = pd.DataFrame(data=people)
+  return people
 
 
 def get_positivity(string):
@@ -71,20 +72,27 @@ sentiment_analysis = pipeline(
     return_all_scores = True
 )
 
-for person in df.columns:
-  df[f'{person}_positivity_index'] = df[person].apply(lambda r: get_positivity(r))
-  df[f'{person}_negativity_index'] = df[person].apply(lambda r: get_negativity(r))
-  df[f'{person}_neutrality_index'] = df[person].apply(lambda r: get_neutrality(r))
+def run():
+  filename = ''
+  total_chat = get_total_chat(filename)
+  people = get_people(total_chat)
+  pd.DataFrame(data=people)
   
-for person in people.keys():
-  df[f'{person}_positivity_score'] = ((df[f'{person}_positivity_index'] + 
-                                         df[f'{person}_negativity_index']) / 2 + 
-                                        df[f'{person}_neutrality_index']) / 2
+  for person in df.columns:
+    df[f'{person}_positivity_index'] = df[person].apply(lambda r: get_positivity(r))
+    df[f'{person}_negativity_index'] = df[person].apply(lambda r: get_negativity(r))
+    df[f'{person}_neutrality_index'] = df[person].apply(lambda r: get_neutrality(r))
+    
+  for person in people.keys():
+    df[f'{person}_positivity_score'] = ((df[f'{person}_positivity_index'] + 
+                                           df[f'{person}_negativity_index']) / 2 + 
+                                          df[f'{person}_neutrality_index']) / 2
+
+  people_scores = []
+  for name in people.keys():
+    score = round(df[f'{name}_positivity_score'].mean(), 5)
+    people_scores.append((name, score))
+  people_scores.sort(reverse=True, key=lambda x:x[1])
+  for person in people_scores:
+    print(f'{person[0]} positivity score: {person[1]}')
   
-people_scores = []
-for name in people.keys():
-  score = round(df[f'{name}_positivity_score'].mean(), 5)
-  people_scores.append((name, score))
-people_scores.sort(reverse=True, key=lambda x:x[1])
-for person in people_scores:
-  print(f'{person[0]} positivity score: {person[1]}')
